345a8021bbc9ffaaa02848502d9affaa5f4097ae
Jenn Ng <jennxf@Jenns-MacBook-Pro.local>
2015-07-12 10:59:34 -0700

    geo for smrt tweets

---
+++ tweet_geo.py
@@ -0,0 +1,22 @@
+21 number of tweets dict.
+[u'smrt', u'outrampark'] {u'type': u'Point', u'coordinates': [1.25420928, 103.81870691]}
+[u'action1D', u'Ashes', u'PARTY', u'SMRT', u'DEVIL', u'trndnl'] None
+[u'SMRT'] {u'type': u'Point', u'coordinates': [1.42308869, 103.84440628]}
+[u'smrt', u'singapore'] None
+[u'smrt', u'singapore'] {u'type': u'Point', u'coordinates': [1.33952588, 103.70563905]}
+[u'NSEW', u'SMRT'] {u'type': u'Point', u'coordinates': [1.40469, 103.90217]}
+[u'haywire', u'smrt'] {u'type': u'Point', u'coordinates': [1.33580905, 103.73907189]}
+[u'smrt'] {u'type': u'Point', u'coordinates': [1.30553027, 103.83100374]}
+[u'smrt'] {u'type': u'Point', u'coordinates': [1.30774486, 103.86291709]}
+[u'smrt'] {u'type': u'Point', u'coordinates': [1.27904234, 103.84315077]}
+[u'SMRT'] {u'type': u'Point', u'coordinates': [1.3337946, 103.74159156]}
+[u'googlemap', u'smrt'] {u'type': u'Point', u'coordinates': [1.3188738, 103.8541052]}
+[u'angry', u'mob', u'commuters', u'smrt'] None
+[u'angry', u'mob', u'commuters', u'smrt'] {u'type': u'Point', u'coordinates': [1.32032831, 103.77042403]}
+[u'smrt'] {u'type': u'Point', u'coordinates': [1.308069, 103.8633098]}
+[u'SMRT', u'fail', u'Singapore'] {u'type': u'Point', u'coordinates': [1.28270613, 103.85003915]}
+[u'smrt'] {u'type': u'Point', u'coordinates': [1.3111481, 103.8707612]}
+[u'smrt'] {u'type': u'Point', u'coordinates': [1.3111516, 103.8707628]}
+[u'smrt'] None
+[u'smrt'] {u'type': u'Point', u'coordinates': [1.3111635, 103.870751]}
+[u'powertrip', u'smrt', u'transportwoes'] {u'type': u'Point', u'coordinates': [1.3691614, 103.8497693]}
